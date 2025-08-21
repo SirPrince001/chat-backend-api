a5a03f831ade1ce0622919df30b21f34a7a15d47
prince bassey <basseyprince75@gmail.com>
2025-08-21 09:13:37 +0100

    updated my database connection to use ssl

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
+
 console.log("ENV:", {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -8,15 +9,20 @@ console.log("ENV:", {
   name: process.env.DB_NAME,
 });
 
-
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
   process.env.DB_PASS as string,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true, // Required by PlanetScale
+      },
+    },
   }
 );
 
